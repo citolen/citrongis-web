81b6394db0b555fdea334496af5bf68b3b3746f2
citolen <nils.citoleux@epitech.eu>
2014-08-16 19:01:53 +0200

    added coffeescript to the extension and completed the test

--- tests/citrongis-app/html/js/index.js
+++ tests/citrongis-app/html/js/index.js
@@ -5,3 +5,8 @@ this.myvar = 'toto';
 this.myfunc = function () {
     console.log('my click');
 };
+
+this.coffee_result = this.CoffeeScript.run('console.log "Hello from coffee-script in citronGIS bitch!"\n\
+times = (a, b) -> a * b\n\
+return times(2, 5)');
+
