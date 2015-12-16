eee868fef039d7b1adadf626a1c8ec4a67a93a48
citolen <nils.citoleux@epitech.eu>
2015-12-16 10:09:11 +0100

    ourmap: fix popup initialization callback

--- src/modules/ourmap/models/Feature.js
+++ src/modules/ourmap/models/Feature.js
@@ -39,19 +39,18 @@ Feature.prototype._click = function (f, evt) {
     if (!this._popup) {
         var self = this;
         this._popup = C.Popup(this._feature._feature, {
-            content: '<button class="edit"><i class="fa fa-pencil"></i></button><button class="delete"><i class="fa fa-trash-o"></i></button>',
-            initialized: function (popup) {
-                popup.$('.edit').click(function () {
-                    self._feature.edit();
-                    self._popup.close();
-                    C.Events.on('mapClicked', self._mapClickedFct);
-                    self.emit('edit', self);
-                });
+            content: '<button class="edit"><i class="fa fa-pencil"></i></button><button class="delete"><i class="fa fa-trash-o"></i></button>'
+        }, function (popup) {
+            popup.$('.edit').click(function () {
+                self._feature.edit();
+                self._popup.close();
+                C.Events.on('mapClicked', self._mapClickedFct);
+                self.emit('edit', self);
+            });
 
-                popup.$('.delete').click(function () {
-                    root.manager.removeFeature(self._id);
-                });
-            }
+            popup.$('.delete').click(function () {
+                root.manager.removeFeature(self._id);
+            });
         });
     }
     if (!this._feature._isEditing) {
